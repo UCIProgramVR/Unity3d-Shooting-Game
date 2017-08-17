b76a2de85b280e1677a05ecb0b928a0ad4eb6f71
Dainel Park <topbladep@gmail.com>
2017-08-17 14:30:11 -0700

    add some resources

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))]
+public class CameraController : MonoBehaviour {
+
+    Vector3 offset;
+    PlayerController controller;
+
+	
+  
+	// Update is called once per frame
+	void Update () {
+		// Temporary vector
+   	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+	Rigidbody myRigidBody;
+	Vector3 velocity;
+
+	void Start()
+	{
+		this.myRigidBody = GetComponent<Rigidbody>();
+	}
+
+	public void Move(Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+
+	public void FixedUpdate()
+	{
+        myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
+	}
+
+	public void LookAt(Vector3 LookPoint)
+	{
+		Vector3 heightCorrectedPoint = new Vector3(LookPoint.x, transform.position.y, LookPoint.z);
+        Camera.main.WorldToScreenPoint(heightCorrectedPoint);
+		transform.LookAt(heightCorrectedPoint);
+	}
+}
